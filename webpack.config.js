224ce0ae058a7f918ddd4e4b50aa2c83f0d65d07
busitora <ts12272530@gmail.com>
2021-02-07 12:36:22 +0900

    add TS

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,19 @@ const path = require("path");
 
 module.exports = {
   // 最初に読み込むファイルを指定
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
   // 出力の設定
   output: {
     // output セクションは、出力するファイルの設定
