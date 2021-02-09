6bb20649ce1577567747933ccbaacad691edc27a
busitora <ts12272530@gmail.com>
2021-02-10 08:16:47 +0900

    add editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,20 @@
-// console.log("Hello, Webpack from busitora + TS!");
-
-//   const log = (message: number): void => {
-//     console.log(message)
-//   }
-
-// const text_log: (message: string) => void = message => {
-//     // void = message っていうのが意味わからん
-//     console.log(message)
-//   }
-
-// log(11)
-// text_log('Hello, Webpack + TypeScript!')
-
-// // void は 何も返却されないという意味
-
 import * as React from "react";
 import { render } from "react-dom";
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { Editor } from './pages/editor';
 
- const Header = styled.h1`
-   color: red;
- `;
+const GlobalStyle = createGlobalStyle`
+    body * {
+      box-sizing: border-box;
+    }
+  `;
 
- const Main = <Header>Markdown Editor</Header>;
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+);
+
 render(Main, document.getElementById("app"));
