004bb5cee11823cf6300c06aa9567c4615006bca
busitora <ts12272530@gmail.com>
2021-02-13 13:14:37 +0900

    header の components 化

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -5,28 +5,10 @@ import { useStateWithStorage } from "../hooks/use_state_with_storage";
 import { putMemo } from "../indexeddb/memos";
 import { Button } from "../components/button";
 import { SaveModal } from "../components/save_modal";
+import { Link } from "react-router-dom";
+import { Header } from "../components/header";
 
 const { useState } = React;
-
-const Header = styled.header`
-  align-content: center;
-  display: flex;
-  font-size: 1.5rem;
-  height: 2rem;
-  justify-content: space-between;
-  left: 0;
-  line-height: 2rem;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`;
-
-const HeaderControl = styled.div`
-  height: 2rem;
-  display: flex;
-  align-content: center;
-`;
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -34,6 +16,13 @@ const Wrapper = styled.div`
   position: fixed;
   right: 0;
   top: 3rem;
+`;
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
 `;
 
 const TextArea = styled.textarea`
@@ -59,6 +48,53 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
+// // const Header = styled.header`
+// //   align-content: center;
+// //   display: flex;
+// //   font-size: 1.5rem;
+// //   height: 2rem;
+// //   justify-content: space-between;
+//   const Wrapper = styled.div`
+//     bottom: 0;
+//     left: 0;
+//     line-height: 2rem;
+//     padding: 0.5rem 1rem;
+//     position: fixed;
+//     right: 0;
+//     top: 3rem;
+// `;
+
+// const Wrapper = styled.div`
+//   bottom: 0;
+//   left: 0;
+//   position: fixed;
+//   right: 0;
+//   top: 3rem;
+// `;
+
+// const TextArea = styled.textarea`
+//   border-right: 1px solid silver;
+//   border-top: 1px solid silver;
+//   bottom: 0;
+//   font-size: 1rem;
+//   left: 0;
+//   padding: 0.5rem;
+//   position: absolute;
+//   top: 0;
+//   width: 50vw;
+// `;
+
+// const Preview = styled.div`
+//   border-top: 1px solid silver;
+//   bottom: 0;
+//   overflow-y: scroll;
+//   padding: 1rem;
+//   position: absolute;
+//   right: 0;
+//   top: 0;
+//   width: 50vw;
+// `;
+
 const StorageKey = "pages/editor:text";
 
 export const Editor: React.FC = () => {
@@ -68,12 +104,12 @@ export const Editor: React.FC = () => {
 
   return (
     <>
-      <Header>
-        Markdown Editor
-        <HeaderControl>
+      <HeaderArea>
+        <Header title="Markdown Editor">
           <Button onClick={() => setShowModal(true)}>保存する</Button>
-        </HeaderControl>
-      </Header>
+          <Link to="/history">履歴を見る</Link>
+        </Header>
+      </HeaderArea>
       <Wrapper>
         <TextArea onChange={(event) => setText(event.target.value)} />
         <Preview>
