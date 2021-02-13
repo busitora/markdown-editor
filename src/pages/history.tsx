7c5a45f8f807b671789062212d5d6c30c45c0eb2
busitora <ts12272530@gmail.com>
2021-02-13 16:06:31 +0900

    add pagenaiton

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { Header } from "../components/header";
-import { getMemos, MemoRecord } from "../indexeddb/memos";
+import { getMemos, getMemoPageCount, MemoRecord } from "../indexeddb/memos";
 
 const { useState, useEffect } = React;
 
@@ -14,12 +14,13 @@ const HeaderArea = styled.div`
 `;
 
 const Wrapper = styled.div`
-  bottom: 0;
+  bottom: 3rem;
   left: 0;
   position: fixed;
   right: 0;
   top: 3rem;
   padding: 0 1rem;
+  overflow-y: scroll;
 `;
 
 const Memo = styled.button`
@@ -44,6 +45,29 @@ const MemoText = styled.div`
   white-space: nowrap;
 `;
 
+const Paging = styled.div`
+  bottom: 0;
+  height: 3rem;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem;
+  position: fixed;
+  right: 0;
+  text-align: center;
+`;
+
+const PagingButton = styled.button`
+  background: none;
+  border: none;
+  display: inline-block;
+  height: 2rem;
+  padding: 0.5rem 1rem;
+
+  &:disabled {
+    color: silver;
+  }
+`;
+
 // export const History: React.FC = () => {
 interface Props {
   setText: (text: string) => void;
@@ -52,13 +76,28 @@ export const History: React.FC<Props> = (props) => {
   const { setText } = props;
   const history = useHistory();
   const [memos, setMemos] = useState<MemoRecord[]>([]);
-  console.log(memos);
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
+//   console.log(memos);
 
   useEffect(() => {
-    getMemos().then(setMemos);
+    getMemos(1).then(setMemos);
+    getMemoPageCount().then(setMaxPage);
   }, []);
   // 初回レンダリングで、getMemos実行してStateに保存、第2引数は今回はなし
   // useEffectは「副作用 (effect) フック」と呼ばれ、レンダリングの後 に実行
+
+  const canNextPage: boolean = page < maxPage; // 最大ページ数未満であれば次ページに遷移可能
+  const canPrevPage: boolean = page > 1; // 2ページ目以降であれば前ページに遷移可能
+  const movePage = (targetPage: number) => {
+    if (targetPage < 1 || maxPage < targetPage) {
+      return;
+    }
+    console.log(targetPage);
+    setPage(targetPage);
+    getMemos(targetPage).then(setMemos);
+  };
+
   return (
     <>
       <HeaderArea>
@@ -80,6 +119,21 @@ export const History: React.FC<Props> = (props) => {
           </Memo>
         ))}
       </Wrapper>
+      <Paging>
+        <PagingButton
+          onClick={() => movePage(page - 1)}
+          disabled={!canPrevPage}
+        >
+          ＜
+        </PagingButton>
+        {page} / {maxPage}
+        <PagingButton
+          onClick={() => movePage(page + 1)}
+          disabled={!canNextPage}
+        >
+          ＞
+        </PagingButton>
+      </Paging>
     </>
   );
 };
