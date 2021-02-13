529bb482c1deb650b048cee8b9d6471791198df3
busitora <ts12272530@gmail.com>
2021-02-13 14:20:41 +0900

    fix

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -48,53 +48,6 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
-// // const Header = styled.header`
-// //   align-content: center;
-// //   display: flex;
-// //   font-size: 1.5rem;
-// //   height: 2rem;
-// //   justify-content: space-between;
-//   const Wrapper = styled.div`
-//     bottom: 0;
-//     left: 0;
-//     line-height: 2rem;
-//     padding: 0.5rem 1rem;
-//     position: fixed;
-//     right: 0;
-//     top: 3rem;
-// `;
-
-// const Wrapper = styled.div`
-//   bottom: 0;
-//   left: 0;
-//   position: fixed;
-//   right: 0;
-//   top: 3rem;
-// `;
-
-// const TextArea = styled.textarea`
-//   border-right: 1px solid silver;
-//   border-top: 1px solid silver;
-//   bottom: 0;
-//   font-size: 1rem;
-//   left: 0;
-//   padding: 0.5rem;
-//   position: absolute;
-//   top: 0;
-//   width: 50vw;
-// `;
-
-// const Preview = styled.div`
-//   border-top: 1px solid silver;
-//   bottom: 0;
-//   overflow-y: scroll;
-//   padding: 1rem;
-//   position: absolute;
-//   right: 0;
-//   top: 0;
-//   width: 50vw;
-// `;
-
 const StorageKey = "pages/editor:text";
 
 export const Editor: React.FC = () => {
